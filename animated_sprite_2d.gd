d8a03d3da2c583173d1bc79fbcf985546dcf607b
prdím <jirikmachula@gmail.com>
2025-02-24 17:18:48 +0100

    kuliska potreti

--- animated_sprite_2d.gd
+++ animated_sprite_2d.gd
@@ -1,11 +1,16 @@
-extends AnimatedSprite2D
+extends Node2D
 @export var speed = 200
-
-# Called when the node enters the scene tree for the first time.
+var _abs_time = 0
+var _initial_pos = position
 func _ready():
-	$".".play("default")
+	$AnimatedSprite2D.play("default")
  
 func _process(delta):
+	self._abs_time += delta
 	var movement = delta * speed
 	self.position = self.position + (Vector2.LEFT * movement)
-	self.position = self.position + (Vector2.UP * sin(self.position.x) * 100)
+	self.position.y = _initial_pos.y + sin(self._abs_time) * 100
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	self.queue_free()
