636e6e82207f33e3a7f3a17ea8072a097460ac83
prdím <jirikmachula@gmail.com>
2025-02-24 17:02:22 +0100

    kulisak se hybe

--- animated_sprite_2d.gd
+++ animated_sprite_2d.gd
@@ -1,7 +1,10 @@
 extends AnimatedSprite2D
-
+@export var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$".".play("default")
  
+func _process(delta):
+	var movement = delta * speed
+	self.position = self.position + (Vector2.LEFT * movement)
