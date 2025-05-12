0a061d5684af7fb058b4eb8989a70e14f1a7b763
jirka <jirikmachula@gmail.com>
2025-05-12 16:07:53 +0200

    zapomel jsem stagnout ostatni

--- Levels/background_level2.gd
+++ Levels/background_level2.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var _pipe_scene=preload ("res://pipe.tscn")
 
 ## Pohyb pozadí v px/s
 @export var speed = 100:
@@ -25,3 +25,15 @@ func apply_speed():
 func _on_area_2d_body_entered(body):
 	if body.has_method("add_damage"):
 		body.add_damage(1000, self)
+
+
+func _on_pipespawntimer_timeout():
+	var screen_size = get_viewport_rect().size
+	var new_pipe = _pipe_scene.instantiate() as Node2D
+	if randi_range(1, 10) >=5:
+		new_pipe.rotation_degrees = 180
+	new_pipe.position.x = screen_size.x + 50
+	new_pipe.position.y = screen_size.y / randi_range(2, 4)
+	new_pipe.speed = self.speed 
+	%pipes.add_child(new_pipe)
+	
