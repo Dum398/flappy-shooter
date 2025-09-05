63ffef061bbb114ea05709d81f74c4a23e8d4c79
jirka <jirikmachula@gmail.com>
2025-09-05 12:32:40 +0200

    Adds credits screen to main menu
    Adds a credits screen accessible from the main menu.
    This allows players to view the game's developers and contributors.
    Also renames 'Button' in main menu to 'Multiplayer'.

--- GUI/mmenu.gd
+++ GUI/mmenu.gd
@@ -24,3 +24,7 @@ func _on_button_pressed():
 
 func _on_texture_button_pressed():
 	get_tree().change_scene_to_file("res://fp.tscn")
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://Levels/credits.tscn")
