36bf02ab1117b2772f6529fd3f7cba8ba010069c
jirka <jirikmachula@gmail.com>
2025-11-09 13:15:44 +0100

    modloader fixed

--- GUI/modload.gd
+++ GUI/modload.gd
@@ -1,7 +1,10 @@
 extends Node
 var mods_path = OS.get_environment("USERPROFILE") + "\\Downloads\\mods\\script.gd"
+var mods = load(mods_path)
 func _ready():
-	var mods = load(mods_path)
-	var instance = mods.new()
-	add_child(instance)
+	if not mods == null:
+		var instance = mods.new()
+		add_child(instance)
+
+
 		
