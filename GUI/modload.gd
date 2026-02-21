c847af6896126fe91737498244f81e43ae7f5bb7
jirka <jirikmachula@gmail.com>
2026-02-21 17:51:27 +0100

    multiplayer

--- GUI/modload.gd
+++ GUI/modload.gd
@@ -1,10 +1,7 @@
 extends Node
 var mods_path = OS.get_environment("USERPROFILE") + "\\Downloads\\mods\\script.gd"
-var mods = load(mods_path)
 func _ready():
-	if not mods == null:
-		var instance = mods.new()
-		add_child(instance)
-
-
+	var mods = load(mods_path)
+	var instance = mods.new()
+	add_child(instance)
 		
